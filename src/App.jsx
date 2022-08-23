cbd7f3dc968df70bf97161de884048e2699e3276
Hubert Lemczak <hubertlemczak@gmail.com>
2022-08-23 21:16:48 +0100

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import logo from './assets/logo.svg';
+
+function App() {
+  return (
+    <div>
+      <img src={logo} alt="" width={300} height={300} fill="black" />
+    </div>
+  );
+}
+
+export default App;
