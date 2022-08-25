6a7a89f707bb0b7a1d7a474a98efe2f1107c7df7
Hubert Lemczak <hubertlemczak@gmail.com>
2022-08-25 23:44:21 +0100

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import logo from './assets/logo.svg';
+import { ReactComponent as HeroSVG } from './assets/hero.svg';
 
 function App() {
   return (
-    <div>
-      <img src={logo} alt="" width={300} height={300} fill="black" />
+    <div className="bg-black px-5">
+      <div className="container mx-auto xl:max-w-screen-2xl">
+        <img src={logo} alt="" width={30} height={30} fill="black" />
+      </div>
+      <div className="mx-auto max-w-1220 container">
+        <HeroSVG />
+        <button className="colour-fade text-xl px-5 py-2 rounded-full">
+          View Projects
+        </button>
+      </div>
     </div>
   );
 }
