dafcb3d67065bb6d4d8111ef8aae38f5c5621462
Hubert Lemczak <hubertlemczak@gmail.com>
2022-09-04 23:20:33 +0100

    feat: projects section component setup

---
+++ src/pages/projects/index.jsx
@@ -0,0 +1,18 @@
+const Projects = () => {
+  return (
+    <section className="mx-auto container lg:max-w-7xl relative">
+      <h2 className="text-275 opactity-15 text-white font-bold" id="about">
+        my projects
+      </h2>
+      <h2 className="text-275 opactity-15 text-white font-bold" id="contact">
+        my projects
+      </h2>
+      <h2 className="text-275 opactity-15 text-white font-bold" id="projects">
+        my projects
+      </h2>
+      <h2 className="text-275 opactity-15 text-white font-bold">my projects</h2>
+    </section>
+  );
+};
+
+export default Projects;
