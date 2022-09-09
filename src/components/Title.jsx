c56eb0e5413f608a7c299dd749852e8015243c66
Hubert Lemczak <hubertlemczak@gmail.com>
2022-09-09 19:08:52 +0100

    refactor: title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,21 @@
+import uniqid from 'uniqid';
+
+const Title = ({ title, className }) => {
+  return (
+    <h2
+      className={`cursor-default select-none max-w-1220 text-7xl text-white font-bold xs:mx-auto xs:text-8xl sm:text-9xl md:text-150 lg:text-200 xl:text-275 ${className}`}
+    >
+      {title.split('').map((char) =>
+        char === ' ' ? (
+          <br key={uniqid()} />
+        ) : (
+          <span key={uniqid()} className="opactity-15 hover:opacity-25">
+            {char}
+          </span>
+        )
+      )}
+    </h2>
+  );
+};
+
+export default Title;
