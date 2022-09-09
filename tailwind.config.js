9f85398945dfc670c44b07cb0d89098bd762a4fd
Hubert Lemczak <hubertlemczak@gmail.com>
2022-09-09 19:07:46 +0100

    set tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
@@ -8,7 +9,29 @@ module.exports = {
         textPara: '#ABAAAA',
         buttonLeft: '#884F1B',
         buttonRight: '#580974',
+        toolBg: '#322B3F',
+        cardBg: '#111111',
       },
+      fontSize: {
+        22: '1.375rem',
+        150: '9.375rem',
+        200: '12.5rem',
+        275: '17.1875rem',
+      },
+      width: {
+        1100: '1100px',
+      },
+      height: {
+        620: '620px',
+      },
+    },
+    screens: {
+      xs: '400px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
     },
   },
   plugins: [],
