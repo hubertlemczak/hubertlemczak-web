817f3d1b4f0fd2b5ce203a142f5636ff034bf2b1
Hubert Lemczak <hubertlemczak@gmail.com>
2022-09-16 17:19:24 +0100

    feat: carousel complete

--- src/pages/about/components/Carousel.jsx
+++ src/pages/about/components/Carousel.jsx
@@ -6,17 +6,22 @@ import JS from '../../../assets/images/js.png';
 import TS from '../../../assets/images/ts.png';
 import NODEJS from '../../../assets/images/nodejs.png';
 import POSTGRES from '../../../assets/images/postgres.png';
-import PRISMA from '../../../assets/images/prisma.svg';
+import PRISMA from '../../../assets/images/prisma.png';
 import STYLED from '../../../assets/images/styled.png';
 import TAILWIND from '../../../assets/images/tailwindcss.png';
 import CSS from '../../../assets/images/css.png';
+import FIGMA from '../../../assets/images/figma.png';
+import GIT from '../../../assets/images/git.png';
+import GITHUB from '../../../assets/images/github.png';
+import JASMINE from '../../../assets/images/jasmine.png';
+import JEST from '../../../assets/images/jest.png';
 
 const Carousel = () => {
   return (
     <div className="carousel">
       <div className="carousel-track">
         <div className="carousel-item">
-          <img src={REACT} alt="" className="h-28" />
+          <img src={REACT} alt="" className="h-36" />
         </div>
         <div className="carousel-item">
           <img src={EXPRESS} alt="" className="h-24" />
@@ -34,10 +39,13 @@ const Carousel = () => {
           <img src={TS} alt="" className="h-52" />
         </div>
         <div className="carousel-item">
+          <img src={GIT} alt="" className="h-48" />
+        </div>
+        <div className="carousel-item">
           <img src={NODEJS} alt="" className="h-28" />
         </div>
         <div className="carousel-item">
-          <img src={TAILWIND} alt="" className="h-36" />
+          <img src={TAILWIND} alt="" className="h-48" />
         </div>
         <div className="carousel-item">
           <img src={PRISMA} alt="" className="h-20" />
@@ -49,7 +57,19 @@ const Carousel = () => {
           <img src={STYLED} alt="" className="h-24" />
         </div>
         <div className="carousel-item">
-          <img src={REACT} alt="" className="h-28" />
+          <img src={JEST} alt="" className="h-36" />
+        </div>
+        <div className="carousel-item">
+          <img src={FIGMA} alt="" className="h-24" />
+        </div>
+        <div className="carousel-item">
+          <img src={GITHUB} alt="" className="h-32" />
+        </div>
+        <div className="carousel-item">
+          <img src={JASMINE} alt="" className="h-28" />
+        </div>
+        <div className="carousel-item">
+          <img src={REACT} alt="" className="h-36" />
         </div>
         <div className="carousel-item">
           <img src={EXPRESS} alt="" className="h-24" />
@@ -67,10 +87,13 @@ const Carousel = () => {
           <img src={TS} alt="" className="h-52" />
         </div>
         <div className="carousel-item">
+          <img src={GIT} alt="" className="h-48" />
+        </div>
+        <div className="carousel-item">
           <img src={NODEJS} alt="" className="h-28" />
         </div>
         <div className="carousel-item">
-          <img src={TAILWIND} alt="" className="h-36" />
+          <img src={TAILWIND} alt="" className="h-48" />
         </div>
         <div className="carousel-item">
           <img src={PRISMA} alt="" className="h-20" />
@@ -80,6 +103,18 @@ const Carousel = () => {
         </div>
         <div className="carousel-item">
           <img src={STYLED} alt="" className="h-24" />
+        </div>{' '}
+        <div className="carousel-item">
+          <img src={JEST} alt="" className="h-36" />
+        </div>
+        <div className="carousel-item">
+          <img src={FIGMA} alt="" className="h-24" />
+        </div>
+        <div className="carousel-item">
+          <img src={GITHUB} alt="" className="h-32" />
+        </div>
+        <div className="carousel-item">
+          <img src={JASMINE} alt="" className="h-28" />
         </div>
       </div>
     </div>
--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,6 +1,5 @@
 import Title from '../../components/Title';
 import Carousel from './components/Carousel';
-// import Skills from './components/Skills';
 
 const About = () => {
   return (
@@ -9,7 +8,7 @@ const About = () => {
       id="about"
     >
       <Title title="about me" />
-      <div className="grid gap-20 max-w-1220 mx-auto pb-32 lg:grid-cols-2 lg:mt-28 xl:gap-32">
+      <div className="grid gap-10 max-w-1220 mx-auto pb-36 lg:gap-20 lg:grid-cols-2 lg:mt-28 xl:gap-32">
         <div className="max-w-sm lg:max-w-none">
           <h3 className="font-bold text-4xl text-textHeading dark:text-yellow-600 my-7 lg:text-5xl lg:mt-0 lg:mb-11">
             My Programming Journey
@@ -34,8 +33,15 @@ const About = () => {
             in this image with Gyazo Pro.Search and copy any text in this image
             with Gyazo Pro.
           </p>
+          <h4 className="font-bold text-sm text-textHeading dark:text-yellow-600 mt-7 mb-2">
+            My Skills
+          </h4>
+          <p className="text-xs text-textPara text-justify">
+            REACT, HTML, CSS, JAVASCRIPT, TYPESCRIPT, GIT, NODEJS, PRISMA,
+            EXPRESS, GITHUB, POSTGRESQL, TAILWINDCSS, STYLED COMPONENTS, FIGMA,
+            JASMINE, JEST
+          </p>
         </div>
-        {/* <Skills /> */}
       </div>
       <Carousel />
     </section>
