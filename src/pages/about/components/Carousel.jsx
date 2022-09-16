7a27f09345d4f5d224ef181f3dd315b10abee0d2
Hubert Lemczak <hubertlemczak@gmail.com>
2022-09-16 14:43:12 +0100

    feat: carousel component

--- src/pages/about/components/Carousel.jsx
+++ src/pages/about/components/Carousel.jsx
@@ -1,10 +1,87 @@
+import './index.css';
+import REACT from '../../../assets/images/react.png';
+import EXPRESS from '../../../assets/images/expressjs.png';
+import HTML from '../../../assets/images/html.png';
+import JS from '../../../assets/images/js.png';
+import TS from '../../../assets/images/ts.png';
+import NODEJS from '../../../assets/images/nodejs.png';
+import POSTGRES from '../../../assets/images/postgres.png';
+import PRISMA from '../../../assets/images/prisma.svg';
+import STYLED from '../../../assets/images/styled.png';
+import TAILWIND from '../../../assets/images/tailwindcss.png';
+import CSS from '../../../assets/images/css.png';
+
 const Carousel = () => {
   return (
-    <div className="rounded-full bg-toolBg px-2 my-10">
-      <p className="text-xs h-4">
-        HTML - CSS - JAVASCRIPT - TYPESCRIPT - NODEJS - REACT - TAILWINDCSS -
-        EXPRESS - PRISMA - POSTGRESQL
-      </p>
+    <div className="carousel">
+      <div className="carousel-track">
+        <div className="carousel-item">
+          <img src={REACT} alt="" className="h-28" />
+        </div>
+        <div className="carousel-item">
+          <img src={EXPRESS} alt="" className="h-24" />
+        </div>
+        <div className="carousel-item">
+          <img src={HTML} alt="" className="h-24" />
+        </div>
+        <div className="carousel-item">
+          <img src={CSS} alt="" className="h-28" />
+        </div>
+        <div className="carousel-item">
+          <img src={JS} alt="" className="h-24" />
+        </div>
+        <div className="carousel-item">
+          <img src={TS} alt="" className="h-52" />
+        </div>
+        <div className="carousel-item">
+          <img src={NODEJS} alt="" className="h-28" />
+        </div>
+        <div className="carousel-item">
+          <img src={TAILWIND} alt="" className="h-36" />
+        </div>
+        <div className="carousel-item">
+          <img src={PRISMA} alt="" className="h-20" />
+        </div>
+        <div className="carousel-item">
+          <img src={POSTGRES} alt="" className="h-40" />
+        </div>
+        <div className="carousel-item">
+          <img src={STYLED} alt="" className="h-24" />
+        </div>
+        <div className="carousel-item">
+          <img src={REACT} alt="" className="h-28" />
+        </div>
+        <div className="carousel-item">
+          <img src={EXPRESS} alt="" className="h-24" />
+        </div>
+        <div className="carousel-item">
+          <img src={HTML} alt="" className="h-24" />
+        </div>
+        <div className="carousel-item">
+          <img src={CSS} alt="" className="h-28" />
+        </div>
+        <div className="carousel-item">
+          <img src={JS} alt="" className="h-24" />
+        </div>
+        <div className="carousel-item">
+          <img src={TS} alt="" className="h-52" />
+        </div>
+        <div className="carousel-item">
+          <img src={NODEJS} alt="" className="h-28" />
+        </div>
+        <div className="carousel-item">
+          <img src={TAILWIND} alt="" className="h-36" />
+        </div>
+        <div className="carousel-item">
+          <img src={PRISMA} alt="" className="h-20" />
+        </div>
+        <div className="carousel-item">
+          <img src={POSTGRES} alt="" className="h-40" />
+        </div>
+        <div className="carousel-item">
+          <img src={STYLED} alt="" className="h-24" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,27 +1,43 @@
 import Title from '../../components/Title';
-import Skills from './components/Skills';
+import Carousel from './components/Carousel';
+// import Skills from './components/Skills';
 
 const About = () => {
   return (
     <section
-      className="flex flex-col mx-auto px-4 relative pt-24 xs:items-center md:px-0 lg:block lg:max-w-none"
+      className="flex flex-col mx-auto px-4 relative pt-24 pb-56 xs:items-center md:px-0 lg:block lg:max-w-none"
       id="about"
     >
       <Title title="about me" />
-      <div className="grid gap-20 max-w-1220 mx-auto lg:grid-cols-2 lg:mt-28 xl:gap-32">
+      <div className="grid gap-20 max-w-1220 mx-auto pb-32 lg:grid-cols-2 lg:mt-28 xl:gap-32">
         <div className="max-w-sm lg:max-w-none">
           <h3 className="font-bold text-4xl text-textHeading dark:text-yellow-600 my-7 lg:text-5xl lg:mt-0 lg:mb-11">
             My Programming Journey
           </h3>
-          <p className="text-textPara leading-8 lg:text-22 lg:leading-10">
+          <p className="text-textPara text-justify leading-8 lg:text-22 lg:leading-10">
+            Search and copy any text in this image with Gyazo Pro.Search and
+            copy any text in this image with Gyazo Pro.Search and copy any text
+            in this image with Gyazo Pro.Search and copy any text in this image
+            with Gyazo Pro.Search and copy any text in this image with Gyazo
+            Pro.Search and copy any text in this image with Gyazo Pro.Search and
+            copy any text in this image with Gyazo Pro.Search and copy any text
+            in this image with Gyazo Pro.Search and copy any text in this image
+            with Gyazo Pro.Search and copy any text in this image with Gyazo
+            Pro.Search and copy any text in this image with Gyazo Pro.Search and
+            copy any text in this image with Gyazo Pro.
+          </p>
+        </div>
+        <div className="max-w-sm lg:max-w-none lg:mt-36">
+          <p className="text-textPara text-justify leading-8 lg:text-22 lg:leading-10">
             Search and copy any text in this image with Gyazo Pro.Search and
             copy any text in this image with Gyazo Pro.Search and copy any text
             in this image with Gyazo Pro.Search and copy any text in this image
             with Gyazo Pro.
           </p>
         </div>
-        <Skills />
+        {/* <Skills /> */}
       </div>
+      <Carousel />
     </section>
   );
 };
