367c5417aefb13c922ce44b1594e96a5f64ac3df
Hubert Lemczak <hubertlemczak@gmail.com>
2022-09-21 00:26:47 +0100

    feat: project pages

---
+++ src/pages/projects/chatApp/index.jsx
@@ -0,0 +1,37 @@
+const ChatApp = () => {
+  return (
+    <section className="relative max-w-5xl mx-auto ">
+      <div className="flex flex-col items-center lg:block py-48 lg:py-64">
+        <div className="max-w-325 mx-auto md:max-w-none px-2">
+          <h1 className="dark:text-textHeading text-6xl max-w-lg md:text-7xl font-bold leading-tight lg:max-w-xl">
+            Delivering quality for your users
+          </h1>
+          <p className="dark:text-textPara md:text-lg max-w-md py-12">
+            I build responsive web apps with a focus on clean and organised
+            code.
+          </p>
+        </div>
+        <div className="max-w-325 mx-auto md:max-w-none px-2">
+          <h1 className="dark:text-textHeading text-6xl max-w-lg md:text-7xl font-bold leading-tight lg:max-w-xl">
+            Delivering quality for your users
+          </h1>
+          <p className="dark:text-textPara md:text-lg max-w-md py-12">
+            I build responsive web apps with a focus on clean and organised
+            code.
+          </p>
+        </div>
+        <div className="max-w-325 mx-auto md:max-w-none px-2">
+          <h1 className="dark:text-textHeading text-6xl max-w-lg md:text-7xl font-bold leading-tight lg:max-w-xl">
+            Delivering quality for your users
+          </h1>
+          <p className="dark:text-textPara md:text-lg max-w-md py-12">
+            I build responsive web apps with a focus on clean and organised
+            code.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ChatApp;
