7ea5cf33a4f67d28b7414b0092aa42d5f8b8634e
Hubert Lemczak <hubertlemczak@gmail.com>
2022-10-18 17:44:38 +0100

    update about me content

--- src/pages/landingPage/about/index.jsx
+++ src/pages/landingPage/about/index.jsx
@@ -8,33 +8,21 @@ const About = () => {
       id="about"
     >
       <Title title="about me" />
-      <div className="grid gap-10 max-w-1220 mx-auto pb-36 lg:gap-20 lg:grid-cols-2 lg:mt-28 xl:gap-32">
+      <div className="grid gap-10 max-w-1220 mx-auto pb-36 lg:gap-20 lg:grid-cols-2 xl:gap-32">
         <div className="max-w-sm lg:max-w-none">
           <h3 className="font-bold text-4xl dark:text-textHeading my-7 lg:text-5xl lg:mt-0 lg:mb-11">
             My Programming Journey
           </h3>
           <p className="dark:text-textPara text-justify leading-8 lg:text-22 lg:leading-10">
-            Hi I&apos;m Hubert, a full-stack dev. I&apos;m a Software Developer
-            based in London. Search and copy any text in this image with Gyazo
-            Pro.Search and copy any text in this image with Gyazo Pro.Search and
-            copy any text in this image with Gyazo Pro.Search and copy any text
-            in this image with Gyazo Pro.Search and copy any text in this image
-            with Gyazo Pro.Search and copy any text in this image with Gyazo
-            Pro.Search and copy any text in this image with Gyazo Pro.Search and
-            copy any text in this image with Gyazo Pro.Search and copy any text
-            in this image with Gyazo Pro.Search and copy any text in this image
-            with Gyazo Pro.Search and copy any text in this image with Gyazo
-            Pro.Search and copy any text in this image with Gyazo Pro.
+            Hi I&apos;m Hubert, a full-stack developer and recent Software
+            Development Academy graduate based in London. Motivated to learn new
+            skills to pursue my passion in the tech industry and deliver high
+            quality software to users.
           </p>
         </div>
         <div className="max-w-sm lg:max-w-none lg:mt-36">
-          <p className="dark:text-textPara text-justify leading-8 lg:text-22 lg:leading-10">
-            Search and copy any text in this image with Gyazo Pro.Search and
-            copy any text in this image with Gyazo Pro.Search and copy any text
-            in this image with Gyazo Pro.Search and copy any text in this image
-            with Gyazo Pro.
-          </p>
-          <h4 className="font-bold text-sm dark:text-textHeading mt-7 mb-2">
+          {/* <p className="dark:text-textPara text-justify leading-8 lg:text-22 lg:leading-10"></p> */}
+          <h4 className="font-bold text-sm dark:text-textHeading {/*mt-7*/} mb-2">
             My Skills
           </h4>
           <p className="text-xs dark:text-textPara text-justify">
