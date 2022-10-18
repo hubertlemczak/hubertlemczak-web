5bb8de1993c67931043e086a127a834c9435735a
Hubert Lemczak <hubertlemczak@gmail.com>
2022-10-18 17:34:31 +0100

    decrease title font size

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import uniqid from 'uniqid';
 const Title = ({ title, className }) => {
   return (
     <h2
-      className={`cursor-default select-none max-w-1220 text-7xl dark:text-white font-bold xs:mx-auto xs:text-8xl sm:text-9xl md:text-150 lg:text-200 xl:text-275 ${className}`}
+      className={`cursor-default select-none max-w-1220 text-6xl dark:text-white font-bold xs:mx-auto xs:text-8xl sm:text-9xl md:text-150 lg:text-200 ${className}`}
     >
       {title.split('').map(char =>
         char === ' ' ? (
