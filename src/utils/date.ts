808b92edfe64568a7d2fc398c95a78b8e2474877
Hubert Lemczak <hubertlemczak@gmail.com>
2024-10-11 19:08:38 +0100

    Homepage almost complete

---
+++ src/utils/date.ts
@@ -0,0 +1,9 @@
+export function isToday(date: Date) {
+  const now = new Date();
+
+  return (
+    date.getDate() === now.getDate() &&
+    date.getMonth() === now.getMonth() &&
+    date.getFullYear() === now.getFullYear()
+  );
+}
