7ae4aae440f3b7c01ab74878fb4bdc63fb9a1134
Hubert Lemczak <hubertlemczak@gmail.com>
2024-10-24 19:27:14 +0100

    Simplify experience date prop

--- src/utils/date.ts
+++
@@ -1,9 +0,0 @@
-export function isToday(date: Date) {
-  const now = new Date();
-
-  return (
-    date.getDate() === now.getDate() &&
-    date.getMonth() === now.getMonth() &&
-    date.getFullYear() === now.getFullYear()
-  );
-}
